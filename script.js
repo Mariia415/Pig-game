66a6eb17ac0a0ba8abbfba7a5076996a164545a9
Mariia415 <mariia.galitsyna@gmail.com>
2021-01-22 14:43:53 +0100

    Add players names

--- script.js
+++ script.js
@@ -9,28 +9,67 @@ const globalScoreP0 = document.querySelector('.score--0');
 const globalScoreP1 = document.querySelector('.score--1');
 const player0 = document.querySelector('.player--0');
 const player1 = document.querySelector('.player--1');
-console.log(btnRoll, btnHold, btnNew, player0, player1);
+const name0 = document.querySelector('.name--0');
+const name1 = document.querySelector('.name--1');
+// console.log(btnRoll, btnHold, btnNew, player0, player1);
+console.log(name0, name1);
 
 let activePlayer = 0;
 let currentScore = 0;
 let scores = [0, 0];
-let playing = true;
+let playing = false;
 
 const init = function () {
-  playing = true;
   activePlayer = 0;
   currentScore = 0;
   scores = [0, 0];
+  document.querySelector('.player--0 h2').textContent = 'Player1';
+  document.querySelector('.player--1 h2').textContent = 'Player2';
+  name0.value = '';
+  name1.value = '';
   diceElement.classList.add('hidden');
-  player0.classList.add('player-active');
+  // player0.classList.add('player-active');
+  // player1.classList.remove('player-active');
+  player0.classList.remove('player-active');
   player1.classList.remove('player-active');
   player0.classList.remove('player-winner');
   player1.classList.remove('player-winner');
+  btnRoll.classList.remove('action-button');
+  btnNew.classList.add('action-button');
   currentScoreP0.textContent = 0;
   currentScoreP1.textContent = 0;
   globalScoreP0.textContent = 0;
   globalScoreP1.textContent = 0;
+  name0.classList.remove('hidden');
+  name1.classList.remove('hidden');
+  name0.addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+      document.querySelector('.player--0 h2').textContent = name0.value;
+      name0.classList.add('hidden');
+    }
+  });
 };
+name1.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') {
+    document.querySelector('.player--1 h2').textContent = name1.value;
+    name1.classList.add('hidden');
+  }
+  playing =
+    document.querySelector('.player--0 h2').textContent === name0.value &&
+    document.querySelector('.player--1 h2').textContent === name1.value
+      ? true
+      : false;
+  if (
+    document.querySelector('.player--0 h2').textContent === name0.value &&
+    document.querySelector('.player--1 h2').textContent === name1.value
+  ) {
+    btnNew.classList.remove('action-button');
+    btnRoll.classList.add('action-button');
+
+    player0.classList.add('player-active');
+  }
+});
+
 const switchPlayer = function () {
   document.querySelector(`.current--${activePlayer}`).textContent = 0;
   currentScore = 0;
@@ -40,7 +79,7 @@ const switchPlayer = function () {
 };
 
 //Starting conditions
-init();
+// init();
 
 //Rolling the dice
 btnRoll.addEventListener('click', function () {
@@ -64,7 +103,7 @@ btnRoll.addEventListener('click', function () {
 btnHold.addEventListener('click', function () {
   if (playing) {
     scores[activePlayer] += currentScore;
-    if (scores[activePlayer] < 100) {
+    if (scores[activePlayer] < 20) {
       document.querySelector(`.score--${activePlayer}`).textContent =
         scores[activePlayer];
       switchPlayer();
@@ -81,6 +120,10 @@ btnHold.addEventListener('click', function () {
       playing = false;
       document.querySelector(`.win${activePlayer}`).textContent = 'Winner';
       document.querySelector(`.current--${activePlayer}`).textContent = '🎉';
+      btnNew.classList.add('action-button');
+      btnRoll.classList.remove('action-button');
+      name0.classList.add('hidden');
+      name1.classList.add('hidden');
     }
   }
 });
